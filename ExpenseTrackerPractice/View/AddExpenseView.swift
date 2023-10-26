966b21cff2236dd7f5e2463731b15cc9439b5344
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-26 13:14:29 -0600

    --Progress: Now adding sort, CRUD operations for Expenses

---
+++ ExpenseTrackerPractice/View/AddExpenseView.swift
@@ -0,0 +1,18 @@
+//
+//  AddExpenseView.swift
+//  ExpenseTrackerPractice
+//
+//  Created by Mitch Andrade on 10/26/23.
+//
+
+import SwiftUI
+
+struct AddExpenseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddExpenseView()
+}
