f49ccc5e73c9b54e0409c656227425093349dd1a
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-26 16:34:37 -0600

    -- Progress: Added card View , formatting again on currency string, "@Transient"

---
+++ ExpenseTrackerPractice/View/ExpenseCardView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpenseCardView.swift
+//  ExpenseTrackerPractice
+//
+//  Created by Mitch Andrade on 10/26/23.
+//
+
+import SwiftUI
+
+struct ExpenseCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpenseCardView()
+}
