479d3cbee9aa4fe5e4077f96319b6b4c1e695375
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-26 13:24:17 -0600

    --Break: --frist break from Deep Work

--- ExpenseTrackerPractice/View/AddExpenseView.swift
+++ ExpenseTrackerPractice/View/AddExpenseView.swift
@@ -6,8 +6,19 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct AddExpenseView: View {
+    @Environment(\.dismiss) private var dismiss
+    @Environment(\.modelContext) private var context
+    
+    /// View Properties
+    @State private var title: String = ""
+    @State private var subTitle: String = ""
+    @State private var date: Date = .init()
+    @State private var amount: CGFloat = 0
+    @State private var category: Category?
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
