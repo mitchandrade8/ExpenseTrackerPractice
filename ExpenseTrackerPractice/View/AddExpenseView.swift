cb306896abe80708741497d15f45a5d162a03ff7
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-26 14:48:48 -0600

    -- Progress: added a bunch of logic --> Disabled the add Button until all textfeilds are filed with information, formatter for the value and decimalPad

--- ExpenseTrackerPractice/View/AddExpenseView.swift
+++ ExpenseTrackerPractice/View/AddExpenseView.swift
@@ -19,9 +19,54 @@ struct AddExpenseView: View {
     @State private var amount: CGFloat = 0
     @State private var category: Category?
     
+    /// Categories
+    @Query(animation: .snappy) private var allCategories: [Category]
+    
     var body: some View {
         NavigationStack {
             List {
+                Section("Title") {
+                    TextField("Magic Keyboard", text: $title)
+                }
+                
+                Section("Description") {
+                    TextField("Bought a keyboard at the Apple Store", text: $subTitle)
+                }
+                
+                Section("Amount Spent") {
+                    
+                    HStack(spacing: 4) {
+                        Text("$")
+                            .fontWeight(.semibold)
+                        
+                        TextField("0.0", value: $amount, formatter: formatter)
+                            .keyboardType(.numberPad)
+                    }
+                }
+                
+                Section("Date") {
+                    DatePicker("", selection: $date, displayedComponents: [.date])
+                        .datePickerStyle(.graphical)
+                        .labelsHidden()
+                }
+                
+                /// Category Picker
+                if !allCategories.isEmpty {
+                    HStack {
+                        Text("Category")
+                        
+                        Spacer()
+                        
+                        Picker("", selection: $category) {
+                            ForEach(allCategories) {
+                                Text($0.categoryName)
+                                    .tag($0)
+                            }
+                        }
+                        .pickerStyle(.menu)
+                        .labelsHidden()
+                    }
+                }
                 
             }
             .navigationTitle("Add Expense")
@@ -36,12 +81,30 @@ struct AddExpenseView: View {
                 
                 ToolbarItem(placement: .topBarTrailing) {
                     Button("Add") {
-                        
+                        addExpense()
                     }
+                    .disabled(isAddButtonDisabled)
                 }
             }
         }
+    }
+    
+    /// Disabling Add Button, until  all data has been entered
+    var isAddButtonDisabled: Bool {
+        return title.isEmpty || subTitle.isEmpty || amount == .zero
+    }
+    
+    /// Adding Expense to the Swift Data
+    func addExpense() {
         
+    }
+    
+    /// Decimal Formatter
+    var formatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 2
+        return formatter
     }
 }
 
