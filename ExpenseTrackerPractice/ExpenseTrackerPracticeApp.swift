5f38555956bef5302fc9426ef7b6b52d9b52a052
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-25 18:13:51 -0600

    Initial Commit

---
+++ ExpenseTrackerPractice/ExpenseTrackerPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerPracticeApp.swift
+//  ExpenseTrackerPractice
+//
+//  Created by Mitch Andrade on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
