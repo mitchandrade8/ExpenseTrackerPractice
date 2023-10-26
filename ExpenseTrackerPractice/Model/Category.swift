cf8dbe3d37b913fafcd9f5b9c1f87f72ca2fb3a7
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-26 11:32:23 -0600

    --Updating: to current spot with adding a Relationship between categories and expense

--- ExpenseTrackerPractice/Model/Category.swift
+++ ExpenseTrackerPractice/Model/Category.swift
@@ -5,4 +5,18 @@
 //  Created by Mitch Andrade on 10/26/23.
 //
 
-import Foundation
+import SwiftUI
+import SwiftData
+
+@Model
+class Category {
+    var categoryName: String
+    /// Category Expenses
+    
+    @Relationship(deleteRule: .cascade, inverse: \Expense.category)
+    var expenses: [Expense]?
+    
+    init(categoryName: String) {
+        self.categoryName = categoryName
+    }
+}
